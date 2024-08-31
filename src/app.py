a175360eea7bee330e5783e44b0477c94ca5c534
Gustavo Pacheco <gustavonf2005@gmail.com>
2024-08-31 11:50:50 -0300

    Feito o Detalhamento do portifolio com icons, e imagens para dar vida ao site

---
+++ src/app.py
@@ -0,0 +1,17 @@
+from flask import Flask, render_template, redirect, url_for
+
+app = Flask(__name__)
+
+# Rota principal que redireciona para a página inicial '/home'
+@app.route('/')
+def index():
+    return redirect(url_for('home'))
+
+# Rota para a página inicial
+@app.route('/home')
+def home():
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    # Executa o aplicativo Flask com o modo de depuração ativado
+    app.run(debug=True)
